e1aa846123a6e2f0909aa247ce2104278c6824a0
mthsgrc <matheus.dev.garcia@gmail.com>
2020-10-27 16:09:53 -0300

    Remade classes. Players and board are created. Board update method done.

--- main.rb
+++ main.rb
@@ -1,113 +1,111 @@
 #!/usr/bin/env ruby
 
-class Player
-  attr_accessor :player_board
+# class TicTac
 
-  def initialize(name)
-    @name = name
-
-  end
-
-  def player_board(move)
-    @player_board = [
-      ["1", "2", "3"],
-      ["4", "5", "6"],
-      ["7", "8", "9"]
-    ]
-
-  end
-
-end
-
-class Board
-  def initialize
-    create_board
-
-  end
-
-  def create_board
-    @board = [
-      ["1", "2", "3"],
-      ["4", "5", "6"],
-      ["7", "8", "9"]
-    ]
-    # board = [
-    #   ["|-|", "|-|", "|-|"],
-    #   ["|-|", "|-|", "|-|"],
-    #   ["|-|", "|-|", "|-|"]
-    # ]
-
-    puts "Initializing Tic Tac Toe Game..."
-    puts "Select a number to play:"
-    puts
-
-    @board.each_with_index do |value, index|
-      puts "#{value} "
-    end
-  end
-
-  def update_board
-
-  end
-
-end
-
+require "pry"
 
 class Match
+  attr_reader :player1, :player2
 
-  @@WIN_CONDITIONS = [
+  def initialize
+    print "Insert name of P1: "
+    player1 = gets.chomp.to_s
+    @player1 = Player.new(player1, "x")
+
+    print "Insert name of P2: "
+    player2 = gets.chomp.to_s
+    @player2 = Player.new(player2, "o")
+
+    @board = Board.new
+    player_turns
+  end
+
+  def player_turns
+    move = 0
+    until move >= 1 && move <= 9
+      print "select a tile with the matching number: "
+      move = gets.chomp.to_i
+    end
+    move = move.to_s
+    # binding.pry
+    @board.update_board(@player1, move)
+  end
+
+  def check_victory
+    @WIN_CONDITIONS =[
+      (@board.tiles[0][0] == "x" && @board.tiles[0][1] == "x" && @board.tiles[0][2] == "x") ||
+      (@board.tiles[0][0] == "o" && @board.tiles[0][1] == "o" && @board.tiles[0][2] == "o") ||
+
+      (@board.tiles[1][0] == "x" && @board.tiles[1][1] == "x" && @board.tiles[1][2] == "x") ||
+      (@board.tiles[1][0] == "o" && @board.tiles[1][1] == "o" && @board.tiles[1][2] == "o") ||
+
+      (@board.tiles[2][0] == "x" && @board.tiles[2][1] == "x" && @board.tiles[2][2] == "x") ||
+      (@board.tiles[2][0] == "o" && @board.tiles[2][1] == "o" && @board.tiles[2][2] == "o") ||
+
+      (@board.tiles[0][0] == "x" && @board.tiles[1][0] == "x" && @board.tiles[2][0] == "x") ||
+      (@board.tiles[0][0] == "o" && @board.tiles[1][0] == "o" && @board.tiles[2][0] == "o") ||
+
+      (@board.tiles[0][1] == "x" && @board.tiles[1][1] == "x" && @board.tiles[2][1] == "x") ||
+      (@board.tiles[0][1] == "o" && @board.tiles[1][1] == "o" && @board.tiles[2][1] == "o") ||
+
+      (@board.tiles[0][2] == "x" && @board.tiles[1][2] == "x" && @board.tiles[2][2] == "x") ||
+      (@board.tiles[0][2] == "o" && @board.tiles[1][2] == "o" && @board.tiles[2][2] == "o") ||
+
+      (@board.tiles[0][0] == "x" && @board.tiles[1][1] == "x" && @board.tiles[2][2] == "x") ||
+      (@board.tiles[0][0] == "o" && @board.tiles[1][1] == "o" && @board.tiles[2][2] == "o") ||
+
+      (@board.tiles[0][2] == "x" && @board.tiles[1][1] == "x" && @board.tiles[2][0] == "x") ||
+      (@board.tiles[0][0] == "o" && @board.tiles[1][1] == "o" && @board.tiles[2][0] == "o")
+    ]
+  end
+
+end
 
 
-  ]
+class Player
+  attr_reader :name, :signal
+  def initialize(name, signal)
+    @name = name
+    @signal = signal
+  end
 
-  # print "Insert name P1:"
-  # player1_name = gets.chomp.to_s
-  # puts "P1 name is: #{player1_name}"
-  # player1 = Player.new(player1_name)
 
-  # print "Insert name P2:"
-  # player2_name = gets.chomp.to_s
-  # puts "P2 name is: #{player2_name}"
-  # player2 = Player.new(player2_name)
+end
 
-  game = Board.new
 
-  def player_turn
-    winner = false
+class Board
+  attr_accessor :tiles
 
-    while winner == false
-
-      puts "Insert a number from 1 to 9 to play that spot."
-      move = player1.gets.chomp
-
-      case move
-      when move == "1" then move = [0][0]
-      when move == "2" then move = [0][1]
-      when move == "3" then move = [0][2]
-      when move == "4" then move = [1][0]
-      when move == "5" then move = [1][1]
-      when move == "6" then move = [1][2]
-      when move == "7" then move = [2][0]
-      when move == "8" then move = [2][1]
-      when move == "9" then move = [2][2]
-      else player_turn
-      end
+  def initialize
+    @tiles = [
+      ["1", "2", "3"],
+      ["4", "5", "6"],
+      ["7", "8", "9"]
+    ]
+    @tiles.each do |value|
+      puts "#{value}"
     end
   end
 
+  def update_board(player, move)
+    binding.pry
+    case move
+    when "1" then self.tiles[0][0] = player.signal
+    when "2" then self.tiles[0][1] = player.signal
+    when "3" then self.tiles[0][2] = player.signal
+    when "4" then self.tiles[1][0] = player.signal
+    when "5" then self.tiles[1][1] = player.signal
+    when "6" then self.tiles[1][2] = player.signal
+    when "7" then self.tiles[2][0] = player.signal
+    when "8" then self.tiles[2][1] = player.signal
+    when "9" then self.tiles[2][2] = player.signal
+    else puts "ERROR"
 
-  def process_move(move)
-
+    end
+    @tiles.each do |value|
+      puts "#{value}"
+    end
   end
-
 end
 
 Match.new
-
-
-
-
-
-# puts board.length
-# puts board[2][0]
-# p board
